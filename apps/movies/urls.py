3e4835998914b9feea95caebe4c648bc637d1e92
bezbak <Cemetey.2019@gmail.com>
2022-12-21 05:31:59 +0600

    index 100% and search system

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.movies.views import movie_detail
+from apps.movies.views import movie_detail, movie_search
 
 urlpatterns = [
-    path('<int:id>/', movie_detail, name = 'movie_detail')
+    path('<int:id>/', movie_detail, name = 'movie_detail'),
+    path('movie_search/', movie_search, name = 'movie_search')
 ]
--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from apps.movies.models import Movie,Comments, Reviews, CommentLikes, CommentDisLikes
+from django.db.models import Q
 from apps.settings.models import Setting
+from datetime import date
 # Create your views here.
 def movie_detail(request, id):
     setting = Setting.objects.latest('id')
@@ -12,19 +14,28 @@ def movie_detail(request, id):
     for rev in movie.review_movie.all():
         review1 +=1
         rev1 +=rev.number
-    review = rev1 / review1
+    try:
+        review = rev1/ review1
+    except:
+        review = 10
     reviews = {}
     # ! Рейтинт для рекомендаций среднее значение
-    for movie in recomandations:
+    for movie2 in recomandations:
         rev_count = 0
         rev_num = 0
-        for rev in movie.review_movie.all():
+        for rev in movie2.review_movie.all():
             rev_count += 1
             rev_num += rev.number
-        rev_mid = rev_num/ rev_count
+        try:
+            rev_mid = rev_num/ rev_count
+        except:
+            rev_mid = 10
         reviews.setdefault(movie.title, str(rev_mid)[:4])
     if request.method == 'POST':
+        movie = Movie.objects.get(id = id)
+        
         if 'comment' in request.POST:
+            movie = Movie.objects.get(id = id)
             text = request.POST.get('text')
             user = request.user
             parent = request.POST.get('parent')
@@ -37,6 +48,7 @@ def movie_detail(request, id):
                 comment.save()
                 return redirect('movie_detail', movie.id)
         if 'like' in request.POST or 'son_like' in request.POST:
+            movie = Movie.objects.get(id = id)
             comment_id = request.POST.get('comment_id')
             try:
                 like = CommentDisLikes.objects.get(user = request.user, comment_id = comment_id)
@@ -54,6 +66,7 @@ def movie_detail(request, id):
                     CommentLikes.objects.create(user = request.user, comment_id = comment_id)
             return redirect('movie_detail', movie.id)
         if 'dislike' in request.POST or 'son_dislike' in request.POST:
+            movie = Movie.objects.get(id = id)
             comment_id = request.POST.get('comment_id')
             try:
                 like = CommentLikes.objects.get(user = request.user, comment_id = comment_id)
@@ -76,7 +89,7 @@ def movie_detail(request, id):
             rating = request.POST.get('rating')
             review = Reviews.objects.create(user = request.user, movie = movie, title = title, text = text, number = rating)
             review.save()
-            return redirect('movie_detail_reviews', movie.id)
+            return redirect('movie_detail', movie.id)
     context = {
         'setting': setting,
         'movie': movie,
@@ -86,3 +99,28 @@ def movie_detail(request, id):
     }
     
     return render(request, 'main/details1.html', context)
+
+def movie_search(request):
+    movies = Movie.objects.all()
+    setting = Setting.objects.latest('id')
+    search_key = request.GET.get('key')
+    reviews = {}
+    for movie in movies:
+        rev_count = 0
+        rev_num = 0
+        for rev in movie.review_movie.all():
+            rev_count += 1
+            rev_num += rev.number
+        try:
+            rev_mid = rev_num/ rev_count
+        except:
+            rev_mid = 10
+        reviews.setdefault(movie.title, str(rev_mid)[:4])
+    if search_key:
+        movies = Movie.objects.filter(Q(title__icontains = search_key) | Q(description__icontains = search_key))
+    context = {
+        'setting':setting,
+        'movies':movies,
+        'reviews':reviews,
+    }
+    return render(request, 'main/search.html', context)
